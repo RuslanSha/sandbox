3e47813b64787f1da5cce7ae1c353fede95d42f5
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-11 13:30:36 +0300

    First commit

---
+++ src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -0,0 +1,34 @@
+package ru.stqa.geometry.figures;
+
+import java.util.Objects;
+
+public record Rectangle(double a, double b) {
+
+    public Rectangle {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Rectangle side should be non-negative");
+        }
+    }
+    public static void printRectangleArea(double a, double b) {
+        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleAreA(a,b));
+        System.out.println(text);
+    }
+
+    private static double rectangleAreA(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(this.a, rectangle.a) == 0 && Double.compare(this.b, rectangle.b) == 0)
+                || (Double.compare(this.b, rectangle.a) == 0 && Double.compare(this.a, rectangle.b) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
---
+++ src/main/java/ru/stqa/geometry/figures/Square.java
@@ -0,0 +1,23 @@
+package ru.stqa.geometry.figures;
+
+public record Square (double side) {
+
+    public Square {
+        if (side < 0) {
+            throw new IllegalArgumentException("Square side should be non-negative");
+        }
+    }
+
+    public static void printSquareArea(Square s) {
+        var text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
+        System.out.println(text);
+    }
+
+    public double area() {
+        return this.side * this.side;
+    }
+
+    public double perimeter() {
+        return 4 * this.side;
+    }
+}
